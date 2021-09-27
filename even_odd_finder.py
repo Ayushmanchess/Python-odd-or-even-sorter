a07f294176742ffa217316c937398549c0d6334c
Ayushman Das <89079526+Ayushmanchess@users.noreply.github.com>
2021-09-27 22:33:20 +0530

    Update even_odd_finder.py

--- even_odd_finder.py
+++ even_odd_finder.py
@@ -14,9 +14,9 @@ if (num2 < num1):
 while True:
     num1 = num1 + 1
     if (num1 % 2) == 0:
-        print(num1, "is even")
+        print(round(num1), "is even")
     else:
-        print(num1, "is odd")
+        print(round(num1), "is odd")
     if (num1==num2):
         break
                 
