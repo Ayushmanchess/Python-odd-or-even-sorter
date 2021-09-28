77d1240911b5c6ea277c72fdc752d1f970d83dc5
Ayushman Das <89079526+Ayushmanchess@users.noreply.github.com>
2021-09-28 20:00:16 +0530

    Update even_odd_finder.py

--- even_odd_finder.py
+++ even_odd_finder.py
@@ -1,6 +1,10 @@
 #importing modules (time)
 import time
 
+#setting variables
+num1odd = 0
+num1even = 0
+
 
 #Asks the user for the number range
 num1 = float(input("Enter the first number of the range!"))
@@ -15,8 +19,11 @@ while True:
     num1 = num1 + 1
     if (num1 % 2) == 0:
         print(round(num1), "is even")
+        num1odd = num1odd + 1
     else:
         print(round(num1), "is odd")
+        num1even = num1even + 1
     if (num1==num2):
         break
-                
+print("The sum of all odd numbers is: ", num1odd, "!")    
+print("The sum of all even numbers is: ", num1even, "!")                  
