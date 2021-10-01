970c9e4751e2ae9486715a47a04f9f986f6290cf
Ayushman Das <89079526+Ayushmanchess@users.noreply.github.com>
2021-10-01 19:53:45 +0530

    Update even_odd_finder.py

--- even_odd_finder.py
+++ even_odd_finder.py
@@ -1,15 +1,13 @@
 #setting variables
 num1even = 0
 num1odd = 0 
+
 #Asks the user for the number range
 num1 = float(input("Enter the first number of the range!"))
 num2 = float(input("Enter the last number of the range!"))
 
 if (num2 < num1):
     print ("Invalid input!")
-
-
-
 
 #main calculation
 while True:
@@ -23,7 +21,7 @@ while True:
 
     if (num1==num2):
         break
-
+#prints the total number of odd and even numbers
 print("total number of even numbers: " + num1even)    
 print("total number of odd numbers: " + num1odd)    
                 
